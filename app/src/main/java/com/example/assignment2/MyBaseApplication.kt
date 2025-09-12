cb2aef0da2205880d4d43e71a2f6cf8031620163
emiliell <emilie.lunleung@live.vu.edu.au>
2025-09-13 03:24:24 +1000

    Create base application

---
+++ app/src/main/java/com/example/assignment2/MyBaseApplication.kt
@@ -0,0 +1,12 @@
+package com.example.assignment2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyBaseApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
