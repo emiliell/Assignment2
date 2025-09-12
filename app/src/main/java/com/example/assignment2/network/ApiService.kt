1fa5132e5c4ad13ffeb34ce02c8c8811b6ff3567
emiliell <emilie.lunleung@live.vu.edu.au>
2025-09-12 16:15:34 +1000

    Add post method for class location

--- app/src/main/java/com/example/assignment2/network/ApiService.kt
+++ app/src/main/java/com/example/assignment2/network/ApiService.kt
@@ -3,9 +3,21 @@ package com.example.assignment2.network
 import android.os.Parcelable
 import com.squareup.moshi.Json
 import kotlinx.android.parcel.Parcelize
+import okhttp3.RequestBody
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiService {
+
+
+    @POST ("{campus}/auth")
+    suspend fun login(
+        @Path("campus") campus: String,
+        @Body body: Map<String, String>
+    )
+
 
     @GET("objects")
     suspend fun getAllObjects(): List<ResponseItem>
