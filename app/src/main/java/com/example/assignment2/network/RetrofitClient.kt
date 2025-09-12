73c720b1e4765d1e43b5a3b2caedce5d9fabe3e6
emiliell <emilie.lunleung@live.vu.edu.au>
2025-09-12 15:43:17 +1000

    Reusing retrofit from android_basic_app zip

---
+++ app/src/main/java/com/example/assignment2/network/RetrofitClient.kt
@@ -0,0 +1,31 @@
+package com.example.assignment2.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class RetrofitClient {
+
+    private val BASE_URL = "https://nit3213api.onrender.com/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(client)
+        .build()
+
+    val apiService: RestfulApiDevService = retrofit.create(RestfulApiDevService::class.java)
+}
