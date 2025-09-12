18977358d672cffb1d375dd7a644d8a89f6f3286
emiliell <emilie.lunleung@live.vu.edu.au>
2025-09-13 03:58:57 +1000

    Add nav and recycler

--- app/src/main/java/com/example/assignment2/ui/recyclerview/DashboardFragment.kt
+++ app/src/main/java/com/example/assignment2/ui/recyclerview/DashboardFragment.kt
@@ -8,10 +8,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.assignment2.R
+import com.example.assignment2.network.ResponseItem
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DashboardFragment : Fragment() {
+
+    private val viewModel: DashboardViewModel by viewModels()
+    private lateinit var navigationFunctionLambda: (ResponseItem) -> Unit
+    private lateinit var recyclerViewAdapter: MyRecyclerViewAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,5 +33,27 @@ class DashboardFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navigationFunctionLambda = {
+            findNavController().navigate(
+                DashboardFragmentDirections.actionDashboardFragmentToDetailsFragment()
+            )
+        }
+        recyclerViewAdapter = MyRecyclerViewAdapter(navigationFunction = navigationFunctionLambda)
+
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+                viewModel.apiResponseObjects.collect { listOfResponseItems ->
+                    recyclerViewAdapter.setData(listOfResponseItems)
+                }
+            }
+        }
+
+        view.findViewById<RecyclerView>(R.id.recyclerView).adapter = recyclerViewAdapter
+    }
 
 }
