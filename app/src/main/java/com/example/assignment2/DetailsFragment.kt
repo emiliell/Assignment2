266f66ed70b31a1c568f33f33cc1445f4dce5d72
emiliell <emilie.lunleung@live.vu.edu.au>
2025-09-13 21:56:30 +1000

    Use DetailsFragmentAgrs for navigation and set json values

--- app/src/main/java/com/example/assignment2/DetailsFragment.kt
+++ app/src/main/java/com/example/assignment2/DetailsFragment.kt
@@ -5,11 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
+import com.example.assignment2.network.ResponseItem
+
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment() {
 
+    private val arguments: DetailsFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +27,21 @@ class DetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val detail = arguments.details
+
+        view.findViewById<TextView>(R.id.textArtistName)?.text = "Artist Name: ${detail.artist_name ?: "NA"}"
+        view.findViewById<TextView>(R.id.textAlbumTitle)?.text = "Album Title: ${detail.album_title ?: "NA"}"
+        view.findViewById<TextView>(R.id.textReleaseYear)?.text = "Release Year: ${detail.release_year ?: "NA"}"
+        view.findViewById<TextView>(R.id.textGenre)?.text = "Genre: ${detail.genre ?: "NA"}"
+        view.findViewById<TextView>(R.id.textTrackCount)?.text = "Track Count: ${detail.track_count ?: "NA"}"
+        view.findViewById<TextView>(R.id.textPopularTrack)?.text = "Popular Track: ${detail.popular_track ?: "NA"}"
+        view.findViewById<TextView>(R.id.textDescription)?.text = "Description: ${detail.description ?: "NA"}"
+
     }
 
 
